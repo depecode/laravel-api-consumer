ec7c48d28efca2e7d66f9e3839ad32816fa19d41
Ajani Opeyemi <ooajani@outlook.com>
2020-05-16 09:48:43 +0100

    Upgrade to V5 to V7

--- src/Provider/LaravelServiceProvider.php
+++ src/Provider/LaravelServiceProvider.php
@@ -6,7 +6,17 @@ use Illuminate\Support\ServiceProvider as BaseProvider;
 use Optimus\ApiConsumer\Router;
 
 class LaravelServiceProvider extends BaseProvider {
+    /***
+     * LaravelServiceProvider constructor.
+     * @param \Illuminate\Foundation\Application $app
+     * needed for laravel upgrade
+     */
+    public function __construct(\Illuminate\Foundation\Application $app)
+    {
+        parent::__construct($app);
 
+    }
+    
     public function register()
     {
 
